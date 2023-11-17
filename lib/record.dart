e1ef7596a305720244a19718e18a30e2a3133334
Aaljani <janicea@metropolia.fi>
2023-11-17 20:20:06 +0200

    database

--- lib/record.dart
+++ lib/record.dart
@@ -1,9 +1,12 @@
 import 'dart:io';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:record/record.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:voice_memo/API/whisper_api.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
 
 late Record audioRecord;
 late AudioPlayer audioPlayer;
@@ -11,6 +14,69 @@ bool isRecording = false;
 bool isPlaying = false;
 String audioPath = '';
 String? transcript;
+late Future<Database> database;
+
+class Transcript {
+  final String audioPath;
+  final String baseText;
+  final String? summaryText;
+  final String? cleanText;
+
+  const Transcript({
+    required this.audioPath,
+    required this.baseText,
+    this.summaryText,
+    this.cleanText,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'audioPath': audioPath,
+      'baseText': baseText,
+      'summaryText': summaryText,
+      'cleanText': cleanText,
+    };
+  }
+}
+
+Future<void> newTranscript(Transcript transcript) async {
+  final db = await database;
+
+  await db.insert(
+    'transcripts',
+    transcript.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<List<Transcript>> transcripts() async {
+  final db = await database;
+
+  final List<Map<String, dynamic>> maps = await db.query('transcripts');
+
+  return List.generate(maps.length, (i) {
+    return Transcript(
+      audioPath: maps[i]['audioPath'] as String,
+      baseText: maps[i]['baseText'] as String,
+    );
+  });
+}
+
+Future<String> get localPath async {
+  final directory = await getApplicationDocumentsDirectory();
+
+  return directory.path;
+}
+
+Future<File> get localFile async {
+  final path = await localPath;
+  return File('$path/${DateTime.now().toString()}.m4a');
+}
+
+Future<void> writeAudio(File audio) async {
+  final file = await localFile;
+  file.writeAsBytes(await audio.readAsBytes());
+}
 
 class RecordPage extends StatefulWidget {
   const RecordPage({super.key});
@@ -49,11 +115,29 @@ class _RecordPageState extends State<RecordPage> {
   }
 
   Future<void> stopRecording() async {
+    final db = openDatabase(
+      join(await getDatabasesPath(), 'transcripts.db'),
+      // When the database is first created, create a table.
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE transcripts(id INTEGER PRIMARY KEY AUTOINCREMENT, audioPath TEXT, baseText TEXT, summaryText TEXT, cleanText TEXT)',
+        );
+      },
+      version: 1,
+    );
+    database = db;
     try {
       String? path = await audioRecord.stop();
       print(path);
+      var audio = File(path!);
+      writeAudio(audio);
+      var newPath = await localFile;
       var req = await requestWhisper(path!, null);
       transcript = req;
+      var newTrns = Transcript(audioPath: newPath.path, baseText: req);
+      List<Transcript> debug = await transcripts();
+      print(debug);
+      newTranscript(newTrns);
       setState(() {
         isRecording = false;
         audioPath = path!;
